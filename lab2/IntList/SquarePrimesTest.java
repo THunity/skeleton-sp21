44500d849c8302186b0117c1fdeea6f7d5f780c8
THunity <zhuogong118@gmail.com>
2024-11-01 20:21:04 +0800

    Update SquarePrimesTest.java

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -13,8 +13,29 @@ public class SquarePrimesTest {
     @Test
     public void testSquarePrimesSimple() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
+        IntList lst1 = IntList.of(32, 33, 34, 35, 36, 37, 38, 39);
+
         boolean changed = IntListExercises.squarePrimes(lst);
+        boolean changed1 = IntListExercises.squarePrimes(lst1);
+
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+
+        assertEquals("32 -> 33 -> 34 -> 35 -> 36 -> 1369 -> 38 -> 39", lst1.toString());
+        assertTrue(changed1);
+    }
+    @Test
+    public void testSquarePrimesByUser1() {
+        IntList lst = IntList.of(14, 15, 16, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 18", lst.toString());
+        assertTrue(!changed);
+    }
+    @Test
+    public void testSquarePrimesByUser2() {
+        IntList lst = IntList.of(1, 2, 3, 4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4", lst.toString());
         assertTrue(changed);
     }
 }
